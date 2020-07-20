b1f139a7f111046a78c55ccaa4199eaa410a0a75
Baran <baran.khan@gmail.com>
2020-07-20 19:46:22 +0530

    all--all

--- user/profile.php
+++ user/profile.php
@@ -16,9 +16,13 @@ include 'menu.php';
 
 include 'conn.php';
 
-	$sql1 = "SELECT `id`, `user_id`, `name`, `email`, `gender`, `dob`, `bio` ,`address`, `img` ,`back_cover`FROM `profile` WHERE user_id = '".$_SESSION['id']."'";
+	$sql1 = "SELECT `id`, `user_id`, `name`, `email`, `gender`, `dob`, `bio` ,`address`, `img` ,`back_cover` , 'phone' FROM `profile` WHERE user_id = '".$_SESSION['id']."'";
 	$a = mysqli_query($conn, $sql1);
 	$row = mysqli_fetch_assoc($a);
+
+	$sql111 = "SELECT `id`, `name`, `email`, `password`, `phone`, `time`, `active`, `banned` FROM `userdata` WHERE id = '".$_SESSION['id']."'";
+	$a11 = mysqli_query($conn, $sql111);
+	$row11 = mysqli_fetch_assoc($a11);
 
 
 
@@ -76,40 +80,30 @@ if ($row['user_id'] == $_SESSION['id']){
 
 
 
-<h5 style="padding-left:1%;padding-top:1%;" class="text-dark">Do You Know <?PHP echo $row['name'].'?';?></h5>
+<h5 style="margin-left:1%;margin-top:1%; margin-bottom:1%; " class="text-dark">Do You Know <?PHP echo $row['name'].'?';?></h5>
 <a class="btn btn-danger" style="height:10%; margin-left:auto;margin-top:auto;margin-bottom:auto;" href="edit.php">Edit</a>
 </div>
 	
 
-</div>
 
-
-<div style="background-color: white;height: 7%;border: solid 1.5px;width: 100%;position: relative;left:0%;top:24.5%;border-right: 0px;border-left: 0px;">
-
-
+<div class="tog">
+<div class = "about">
 
 
 
-<?php
+<h5 style="margin-left:1%;margin-top:1%; margin-bottom:1%; " class="text-dark">About</h5>
 
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; " class="text-dark"><?php echo "Name: ".$row['name'];?></h6>
 
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; " class="text-dark"><?php echo "born on: ".$row['dob'];?></h6>
 
-	$sql1 = "SELECT count(*) as counter FROM `follow` WHERE creator_id = '".$_SESSION['id']."'";
-	$a = mysqli_query($conn, $sql1);
-	$row = mysqli_fetch_assoc($a);
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; " class="text-dark"><?php echo "gender: ".$row['gender'];?></h6>
 
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; " class="text-dark"><?php echo "Address: ".$row['address'];?></h6>
 
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; " class="text-dark"><?php echo "Phone No: ".$row11['phone'];?></h6>
 
-?>
-
-
-
-<h5 style="position: relative;top:15px; left:1%;text-align: left;"><?= $row['counter'].' Followers'?></h5>
-
-
-
-
-
+<!-- <h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; " class="text-dark"><?php echo "Status: ".$row['bio'];?></h6> -->
 
 <?php
 
@@ -126,8 +120,24 @@ if ($row['user_id'] == $_SESSION['id']){
 
 
 
-<h5 style="position: relative;top:-15px; left:1%;text-align: center;"><?= $row['counter'].' Following'?></h5>
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; "><?= 'Followers: '.$row['counter']?></h5>
 
+
+<?php
+
+
+
+	$sql1 = "SELECT count(*) as counter FROM `follow` WHERE creator_id = '".$_SESSION['id']."'";
+	$a = mysqli_query($conn, $sql1);
+	$row = mysqli_fetch_assoc($a);
+
+
+
+?>
+
+
+
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; "><?= 'Following: '.$row['counter']?></h5>
 
 
 
@@ -143,11 +153,49 @@ if ($row['user_id'] == $_SESSION['id']){
 
 ?>
 
-<h5 style="position: relative;top:-47px;text-align: right;padding-right: 1%;"><?= $row['counter'].' Likes'?></h5>
+<h6 style="margin-left:2%;margin-top:2%; margin-bottom:1%; "><?= 'Likes: '.$row['counter']?></h6>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 	$sql1 = "SELECT `id`, `user_id`, `name`, `email`, `gender`, `dob`, `bio` ,`address`, `img` ,`back_cover` , 'phone' FROM `profile` WHERE user_id = '".$_SESSION['id']."'";
+	$a = mysqli_query($conn, $sql1);
+	$row = mysqli_fetch_assoc($a);?>
+
+<div class = "post">
+
+
+<h5><?php echo $row['bio'];?></h5>
+
+
+</div>
+
+
 
 
 
 </div>
+
+
+</div>
+
+
+
+
+</div>
+
 
 
 
@@ -176,10 +224,37 @@ if ($row['user_id'] == $_SESSION['id']){
 	width:170px;height: 170px;border-radius: 80%;object-fit: cover;border: solid white 5px;position: relative;position:absolute;bottom:17%;left:1%;
 }	
 
+.tog{
+	display: flex;
+}
+
+
+.about{
+	height:auto;padding-top:0.1%;padding-bottom:0.1%;position: relative;bottom: -27px;background-color: white; width:250px; height:auto; margin-left:2%; padding:1%;
+}
+
+
+.post{
+	height:auto;padding-top:0.1%;padding-bottom:0.1%;position: relative;bottom: -27px;background-color: white; width:60%; height:auto; margin:; padding:1%;left:2%;
+}
+
 @media only screen and (max-width: 492px) {
 	.img{
 	width:140px;height: 140px;border-radius: 80%;object-fit: cover;border: solid white 5px;position: relative;position:absolute;bottom:17%;left:1%;
 }
+.about{
+	height:auto;padding-top:0.1%;padding-bottom:0.1%;position: relative;bottom: -27px;background-color: white; width:300px; height:auto; margin:auto; padding:1%;
+}
+
+.post{
+	height:1000px;padding-top:0.1%;padding-bottom:0.1%;position: relative;bottom: -40px;background-color: white; width:100%; height:auto; margin:; padding:1%;left:0%;
+}
+
+.tog{
+	display: block;
+}
+
+
 }
 
 html, body {
