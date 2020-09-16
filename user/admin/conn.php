ea1024ffb6274cde2a2a459006027ffffd6f3ea9
Baran <baran.khan@gmail.com>
2020-09-16 09:36:46 +0530

    none

--- user/admin/conn.php
+++ user/admin/conn.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$conn = mysqli_connect("localhost", "root", "12345", "my_fb");
-
+// $conn = mysqli_connect("localhost", "root", "12345", "my_fb");
+$conn = mysqli_connect("localhost", "root", "HT$#H1Y&Y@s45", "my_fb");
 
 
 ?>
